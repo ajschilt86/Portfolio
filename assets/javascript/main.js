93ff6d1838bf9b8402a1390995a56047d20eb981
ajschilt <ajschilt86@gmail.com>
2018-08-03 14:08:17 -0500

    not sure what i changed lol

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,29 +3,28 @@ $(document).keydown(function (e) {
     if (e.keyCode == 27) {
         $(".modal").hide();
         $("body").removeClass();
-        console.log("working")
     }
 });
 //close the modal if the x is clicked on
 $("body").on("click", ".close", function () {
     $(".modal").hide();
     $("body").removeClass();
-    console.log("working")
 });
 //close the modal if the anywhere but the modal is clicked on
 $(".modal-content").on("click", function (event) {
     $(".modal").hide();
     $("body").removeClass();
-    console.log("working")
 });
 
 $(".nav-modal").on("click", function (event) {
     $(".modal").show();
-    $(".modal-text").html("<div class='modal-link'><a href='#aboutMe'>About Me</a></div>"
-    + "<div class='modal-link'><a href='#projects'>Projects</a></div>"
-    + "<div class='modal-link'><a href='#contact'>Contact</a></div>");
+    $(".modal-text").html(
+          "<div class='modal-link'><a href='#aboutMe'>About Me</a></div>"
+        + "<div class='modal-link'><a href='#projects'>Projects</a></div>"
+        + "<div class='modal-link'><a href='#contact'>Contact</a></div>");
+        // + "<div class='modal-icons'><a href='https://github.com/ajschilt86' target='_blank'><img src='assets/images/github_icon.svg' alt='GitHub' class='logo'></a>"
+        // + "<div class='modal-icons'><a href='https://www.linkedin.com/in/anthonyschilt/' target='_blank'><img src='assets/images/li_icon.svg' alt='LinkedIn logo' class='logo'></a></div>");
     $("body").addClass("modal-open");
-    console.log("working")
 });
 
 // $(".button").on("click", function (event) {
