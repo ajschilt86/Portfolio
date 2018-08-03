b0792f6e79e898ae009bc2e0bf4df9106ffc3f8b
ajschilt <ajschilt86@gmail.com>
2018-08-03 10:04:55 -0500

    added animations to modal

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,30 +2,31 @@
 $(document).keydown(function (e) {
     if (e.keyCode == 27) {
         $(".modal").hide();
+        $("body").removeClass();
         console.log("working")
     }
 });
 //close the modal if the x is clicked on
 $("body").on("click", ".close", function () {
     $(".modal").hide();
+    $("body").removeClass();
     console.log("working")
 });
 //close the modal if the anywhere but the modal is clicked on
 $(".modal-content").on("click", function (event) {
     $(".modal").hide();
+    $("body").removeClass();
     console.log("working")
 });
 
 $(".nav-modal").on("click", function (event) {
     $(".modal").show();
-    $(".modal-text").html("<div><a href='#aboutMe'>About Me</a></div>"
-    + "<div><a href='#projects'>Projects</a></div>"
-    + "<div><a href='#contact'>Contact</a></div>");
+    $(".modal-text").html("<div class='modal-link'><a href='#aboutMe'>About Me</a></div>"
+    + "<div class='modal-link'><a href='#projects'>Projects</a></div>"
+    + "<div class='modal-link'><a href='#contact'>Contact</a></div>");
+    $("body").addClass("modal-open");
     console.log("working")
 });
 
 
 AOS.init();
-
-
-1
